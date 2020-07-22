9b1a6f07786ea2371a35060087d2532d29ddd35d
usingbattery <68527151+usingbattery@users.noreply.github.com>
2020-07-22 11:27:05 +0800

    main()

--- main.m
+++ main.m
@@ -1,18 +1,20 @@
 % 清空窗口
-clf;
 clear;
+clf;
 clc;
 % 环境准备
-axis equal;
-hold on;
-xlabel('x');
-ylabel('y');
-zlabel('z');
+axis equal;% 校齐坐标轴
+xlabel('x');% 标识x轴
+ylabel('y');% 标识y轴
+zlabel('z');% 标识z轴
+
 % 参数准备
-fineness=1; % 渲染精细度
-flower_position=[1,2,3]; % 花托位置
-flower_size=1; % 放大倍数
-petal_number=8; % 花瓣数量
-% 画出玫瑰
-rose=Rose(fineness,flower_position,flower_size,petal_number);
-rose.Render();
+fineness=1;% 渲染精细度,分辨率
+flower_position=[0,0,0];% 位置,以花托为准
+flower_size=1;% 放大倍数
+petal_number=10;% 花瓣数量
+calyx_number=4;% 花萼数量
+
+% 生成对象
+rose=Rose(fineness,flower_position,flower_size,petal_number,calyx_number);
+rose.Render();% 渲染图形
